f4c329dd2e5f4fe80672bfca10f85fe7eae11e16
Marius Filip <marius.filip.dev@gmail.com>
2015-07-28 18:33:11 +0100

    Code now nice and clean.

--- src/main/java/enumj/ShareableEnumerator.java
+++ src/main/java/enumj/ShareableEnumerator.java
@@ -89,6 +89,8 @@ public class ShareableEnumerator<E> extends AbstractEnumerator<E> {
         return this;
     }
     
+    // ---------------------------------------------------------------------- //
+    
     @Override
     protected boolean internalHasNext() {
         startEnumerating();
@@ -106,7 +108,7 @@ public class ShareableEnumerator<E> extends AbstractEnumerator<E> {
         isSharedEnumerating = null;
     }
     
-     // --------------------------------------------------------------------- //
+    // ---------------------------------------------------------------------- //
 
     /**
      * Creates an {@link Enumerator} instance that will share the
@@ -117,6 +119,7 @@ public class ShareableEnumerator<E> extends AbstractEnumerator<E> {
     public Enumerator<E> share() {
         return share(1)[0];
     }
+
     /**
      * Creates an array of {@link Enumerator} instances that will share
      * the elements of the current enumerator.
@@ -165,195 +168,4 @@ public class ShareableEnumerator<E> extends AbstractEnumerator<E> {
             source = null;
         }
     }
-
-    /*
-    private Iterator<E> source;
-    private boolean isEnumerating;
-    private boolean isSharing;
-    private boolean isSharedEnumerating;
-
-    private final LinkedList<ShareableElement<E>> buffer;
-    private final WeakHashMap<SharingEnumerator<E>,
-                              ShareableElement<E>> waiting;
-
-    /**
-     * Constructs an shareable enumerator out of a provided {@link Iterator}.
-     * <p>
-     * The elements of the provided {@link Iterator} will be shared without
-     * the original iterator to be traversed more than once.
-     * </p>
-     * @param source {@link Iterator} sharing the elements
-     * @exception IllegalArgumentException <code>source</code> is
-     * <code>null</code>
-     *
-    public ShareableEnumerator(Iterator<E> source) {
-        Utils.ensureNotNull(source, Messages.NULL_ENUMERATOR_SOURCE);
-        this.source = source;
-        this.buffer = new LinkedList<>();
-        this.waiting = new WeakHashMap<>();
-    }
-
-    @Override
-    public ShareableEnumerator<E> asShareable() {
-        return this;
-    }
-
-    /**
-     * Creates a {@link SharingEnumerator} instance that will share the
-     * elements of the current enumerator.
-     *
-     * @return the new {@link SharingEnumerator} instance
-     *
-    public SharingEnumerator<E> share() {
-        return share(1)[0];
-    }
-
-    /**
-     * Creates an array of {@link SharingEnumerator} instances that will share
-     * the elements of the current enumerator.
-     *
-     * @param count the number of {@link SharingEnumerator} to create
-     * @return array of new {@link SharingEnumerator} instances
-     * @exception IllegalArgumentException <code>count</code> is negative
-     * @exception IllegalStateException enumeration has started
-     *
-    public SharingEnumerator<E>[] share(int count) {
-        Utils.ensureNonNegative(count,
-                                Messages.NEGATIVE_ENUMERATOR_EXPECTED_COUNT);
-        if (isEnumerating || isSharedEnumerating) {
-            throw new IllegalStateException(Messages.ILLEGAL_ENUMERATOR_STATE);
-        }
-        Utils.ensureNonNegative(count,
-                                Messages.NEGATIVE_ENUMERATOR_EXPECTED_COUNT);
-        isSharing = true;
-        final SharingEnumerator<E>[] enumerators = new SharingEnumerator[count];
-        for(int i=0; i<count; ++i) {
-            enumerators[i] = newSharingEnumerator();
-            waiting.put(enumerators[i], null);
-        }
-        return enumerators;
-    }
-
-    @Override
-    protected boolean internalHasNext() {
-        if (isSharing || isSharedEnumerating) {
-            throw new IllegalStateException(Messages.ILLEGAL_ENUMERATOR_STATE);
-        }
-        isEnumerating = true;
-        return source.hasNext();
-    }
-    @Override
-    protected E internalNext() {
-        isEnumerating = true;
-        return source.next();
-    }
-    @Override
-    protected void cleanup() {
-        source = null;
-        buffer.clear();
-        waiting.clear();
-    }
-
-    boolean hasNext(ShareableElement<E> consumedPtr) {
-        isSharedEnumerating = true;
-        return consumedPtr != null && consumedPtr.next != null
-               || consumedPtr == null && !buffer.isEmpty()
-               || source.hasNext();
-    }
-    ShareableElement<E> next(SharingEnumerator<E> enumerator,
-                             ShareableElement<E> consumedPtr) {
-        assert !isEnumerating;
-        isSharedEnumerating = true;
-        boolean refAdded = false;
-        boolean ok = false;
-        assert consumedPtr != null || waiting.containsKey(enumerator);
-
-        ShareableElement<E> newPtr = next(consumedPtr);
-        assert consumedPtr == null || consumedPtr.next == newPtr;
-
-        try {
-            newPtr.addRef(enumerator);
-            refAdded = true;
-
-            if (consumedPtr != null) {
-                consumedPtr.release(enumerator);
-            }
-            else {
-                final ShareableElement<E> ptr = waiting.remove(enumerator);
-                assert ptr == null;
-            }
-            ok = true;
-        }
-        catch (Throwable err) {
-            if (refAdded) {
-                newPtr.release(enumerator);
-            }
-
-            assert !buffer.isEmpty();
-            if (newPtr == buffer.getLast()) {
-                final ShareableElement<E> last = buffer.removeLast();
-                assert last == newPtr;
-
-                if (!buffer.isEmpty()) {
-                    buffer.getLast().next = null;
-                }
-            }
-
-            throw err;
-        }
-        finally {
-            if (!buffer.isEmpty() && buffer.getFirst().isFree()) {
-                final ShareableElement<E> head = buffer.removeFirst();
-
-                assert consumedPtr != null;
-                assert consumedPtr == head;
-                assert !ok || head.next != null;
-                assert !ok || !buffer.isEmpty();
-                assert !ok || buffer.getFirst() == newPtr;
-
-                head.next = null;
-            }
-        }
-        return newPtr;
-    }
-
-    private ShareableElement<E> next(ShareableElement<E> consumedPtr) {
-        if (consumedPtr == null) {
-            if (buffer.isEmpty()) {
-                getNextElement();
-            }
-            return buffer.getFirst();
-        }
-        if (consumedPtr.next == null) {
-            assert !buffer.isEmpty();
-            assert consumedPtr == buffer.getLast();
-            getNextElement();
-        }
-        return consumedPtr.next;
-    }
-
-    private void getNextElement() {
-        final ShareableElement<E> nextPtr = newShareableElement(source.next(),
-                                                                waiting);
-        final ShareableElement<E> tail = buffer.isEmpty()
-                                         ? null
-                                         : buffer.getLast();
-        buffer.addLast(nextPtr);
-        if (tail != null) {
-            tail.next = nextPtr;
-        }
-    }
-
-    // ---------------------------------------------------------------------- //
-
-    protected SharingEnumerator<E> newSharingEnumerator() {
-        return new SharingEnumerator(this);
-    }
-    protected ShareableElement<E> newShareableElement(
-            E value,
-            WeakHashMap<SharingEnumerator<E>,
-                        ShareableElement<E>> waiting) {
-        return new ShareableElement(value, waiting);
-    }
-    */
 }
--- src/main/java/enumj/SharingEnumerator.java
+++ src/main/java/enumj/SharingEnumerator.java
@@ -67,32 +67,4 @@ final class SharingEnumerator<E> extends AbstractEnumerator<E> {
         value.clear();
         value = null;
     }
-
-    /*
-    private ShareableEnumerator<E> source;
-    private ShareableElement<E> consumedPtr;
-
-    SharingEnumerator(ShareableEnumerator<E> source) {
-        Utils.ensureNotNull(source, Messages.NULL_ENUMERATOR_SOURCE);
-        this.source = source;
-    }
-
-    @Override
-    protected boolean internalHasNext() {
-        return source.hasNext(consumedPtr);
-    }
-    @Override
-    protected E internalNext() {
-        consumedPtr = source.next(this, consumedPtr);
-        return consumedPtr.getValue();
-    }
-    @Override
-    protected void cleanup() {
-        source = null;
-        if (consumedPtr != null) {
-            consumedPtr.release(this);
-            consumedPtr = null;
-        }
-    }
-    */
 }
