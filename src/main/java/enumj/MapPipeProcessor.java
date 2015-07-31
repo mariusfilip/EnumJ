2f9d336b9bb8c750254cc7c89c2a60de3860c424
Marius Filip <marius.filip.dev@gmail.com>
2015-07-31 13:32:41 +0100

    Nice improvement around aggregation of map() operations.

--- src/main/java/enumj/MapPipeProcessor.java
+++ src/main/java/enumj/MapPipeProcessor.java
@@ -19,8 +19,9 @@ import java.util.function.Function;
  */
 final class MapPipeProcessor<In,Out> extends AbstractPipeProcessor<In,Out> {
 
-    protected LinkedList<Function<In,Out>> mappers;
-    protected Out                          value;
+    private Function<In,Out>             mapper;
+    private LinkedList<Function<In,Out>> mappers;
+    private Out                          value;
 
     /**
      * Constructs a {@code MapPipeProcessor} instance.
@@ -34,29 +35,47 @@ final class MapPipeProcessor<In,Out> extends AbstractPipeProcessor<In,Out> {
      */
     public MapPipeProcessor(Function<In,Out> functor) {
         super(false, false);
-        this.mappers = new LinkedList<>();
-        enqueueMap((Function<Out,Out>)functor);
+        mapper = functor;
     }
 
     // ---------------------------------------------------------------------- //
 
     @Override
     public <R> boolean pushFrontMap(Function<Out,R> mapper) {
-        mappers.addFirst((Function)mapper);
+        ensureMappers();
+        this.mappers.addFirst((Function<In,Out>)mapper);
         return true;
     }
     @Override
     public <R> boolean enqueueMap(Function<Out,R> mapper) {
-        mappers.add((Function)mapper);
+        ensureMappers();
+        this.mappers.add((Function<In,Out>)mapper);
         return true;
+    }
+    private void ensureMappers() {
+        if (mappers == null) {
+            mappers = new LinkedList<>();
+            try {
+                mappers.add(mapper);
+                mapper = null;
+            } catch(Throwable err) {
+                mappers = null;
+                throw err;
+            }
+        }
     }
 
     // ---------------------------------------------------------------------- //
 
     @Override
     public void processInputValue(In value) {
-        for(Function fun : mappers) {
-            value = (In)fun.apply(value);
+        if (mapper != null) {
+            value = (In)mapper.apply(value);
+        }
+        else {
+            for(Function fun : mappers) {
+                value = (In)fun.apply(value);
+            }
         }
         this.value = (Out)value;
     }
